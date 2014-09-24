5a91240cb419dfb4504066b5b9183517bf0dd9be
james112358 <041401076@my.central.wa.edu.au>
2014-09-24 12:42:25 +0800

    Question page
    .

---
+++ CamCan/CamCan/QuestionPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace CamCan
+{
+    public partial class QuestionPage : PhoneApplicationPage
+    {
+
+        public Question testQuestion;
+
+        public QuestionPage()
+        {
+            InitializeComponent();
+            testQuestion = new Question();
+
+            // test data
+            testQuestion.testQuestion();
+
+            ContentPanel.DataContext = testQuestion;
+        }
+
+
+    }
+}
