263fbe4bd678ab06defadd914b42cf730b998786
James <041401093@my.central.wa.edu.au>
2014-10-29 10:02:36 +0800

    Fuck this shit
    No.

--- cam/CamCan_Service/Question.cs
+++ cam/CamCan_Service/Question.cs
@@ -7,5 +7,23 @@ namespace CamCan_Service
 {
     public class Question
     {
+        private int questionID;
+        private String question;
+        private String ansA;
+        private String ansB;
+        private String ansC;
+        private String ansD;
+        private String correctAns;
+
+        public int questionID { get;set;}
+        public String question { get; set; }
+        public String ansB { get; set; }
+        public String ansA { get; set; }
+        public String ansC { get; set; }
+        public String ansD { get; set; }
+        public String correctAns { get; set; }
+
     }
+
+
 }
