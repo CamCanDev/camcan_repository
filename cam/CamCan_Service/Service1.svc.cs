68db08e3e3b234fadd01bb7af304eb36f1f68bd4
mtichell-panicciari <041401090@my.central.wa.edu.au>
2014-11-12 12:19:39 +0800

    Webservice update
    upload

--- cam/CamCan_Service/Service1.svc.cs
+++ cam/CamCan_Service/Service1.svc.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Web.Services;
 using System.Data;
 using MySql.Data.MySqlClient;
-using System.Security.Cryptography;
 
 namespace CamCan_Service
 {
@@ -23,17 +22,12 @@ namespace CamCan_Service
         public UserProfile returnUser(String user, String pass)
         {
             String conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
-            String mD5Password;
 
             using (MySqlConnection cnn = new MySqlConnection(conString))
             {
-
                 cnn.Open();
 
-                // Convert password to MD5
-                mD5Password = CalculateMD5Hash(pass);
-
-                String sql = String.Format("SELECT * FROM dgn6la8u0_users WHERE user_login = \"{0}\" and user_pass = \"{1}\"", user, mD5Password);
+                String sql = String.Format("SELECT name,scenario_completed,password FROM employees WHERE name = \"{0}\" and password = \"{1}\"", user, pass);
                 MySqlDataAdapter da = new MySqlDataAdapter(sql, cnn);
                 DataSet ds = new DataSet();
                 da.Fill(ds, "users");
@@ -47,14 +41,14 @@ namespace CamCan_Service
                         u.name = "";
                         u.password = "";
                     }
-
                     //If dataset contains elements
                     else
                     {
                         foreach (DataRow dr in ds.Tables[0].Rows)
                         {
-                            u.name = Convert.ToString(dr["user_login"]);
-                            u.password = pass;
+                            u.name = Convert.ToString(dr["name"]);
+                            u.completed = Convert.ToInt32(dr["scenario_completed"]);
+                            u.password = Convert.ToString(dr["password"]);
                         }
                     }
                 }
@@ -69,8 +63,6 @@ namespace CamCan_Service
 
 
         //Returns Scenario
-        
-        /* Older vesion using old DB
         [WebMethod]
         public Scenario returnScenario(Int32 id)
         {
@@ -91,7 +83,7 @@ namespace CamCan_Service
                 String sqlQ = String.Format("SELECT question, ansA, ansB, ansC, ansD, correctAns FROM questions WHERE scenarioID ={0}", id);
                 MySqlDataAdapter daQ = new MySqlDataAdapter(sql, cnn);
                 DataSet dsQ = new DataSet();
-                daQ.Fill(dsQ, "question");
+                daQ.Fill(dsQ, "scenarios");
 
                 //Objects
                 Scenario s = new Scenario();
@@ -130,6 +122,7 @@ namespace CamCan_Service
                     s.questionArray = questArray;
                 }
 
+
                 catch (Exception ex)
                 {
                     s.scenarioInformation = ex.ToString();
@@ -138,65 +131,6 @@ namespace CamCan_Service
                 return s;
             }
         }
-
-        */
-
-       /* public Scenario returnScenario(int scenarioNum)
-        {
-            
-            String conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
-            Scenario s = new Scenario();
-            s.scenarioID = scenarioNum;
-            Question[] q = new Question[4];
-            String bigString;
-
-            using (MySqlConnection cnn = new MySqlConnection(conString))
-            {
-             
-                cnn.Open();
-
-                //Get html text from database
-                String sql = String.Format("SELECT question FROM dgn6la8u0_frm_fields where id ={0}", scenarioNum);
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, cnn);
-                DataSet ds = new DataSet();
-                da.Fill(ds, "scenarios");
-
-                //
-                try
-                {
-                    //get html into the big string
-                    foreach (DataRow dr in ds.Tables[0].Rows)
-                    {
-                        bigString = Convert.ToString(dr["question"]);
-                    }
-
-                    //Video Link
-                    s.videoLink  = getVideoLink(bigString);
-
-                    //Text
-                    s.scenarioInformation = getScenarionText(bigString);
-
-                    //Question 1
-                    q = getQuestion(scenarioNum);
-
-                    //Add questions to scenario
-                    s.questionArray = q;
-
-                    //Return Scenario
-                    return s;
-                }
-                catch(Exception ex)
-                {
-                    s.scenarioInformation = ex.ToString();
-                }
-
-
-                return s;
-
-            }
-
-        } */
-
         
         //gets user and password and returns the scenario completed
         [WebMethod]
@@ -249,270 +183,6 @@ namespace CamCan_Service
             }
         }
 
-      /*  [WebMethod]
-        public Results showResults(int scenarioID, int employeeID)
-        {
-            //Creating the connection string
-            String conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
-            //Accessing the database via the connection string
-            using (MySqlConnection cnn = new MySqlConnection(conString))
-            {
-                cnn.Open();
-                String sql = String.Format("SELECT scenarioID, questionID, answer, correct FROM results WHERE employeeID = {0} AND ScenarioID = {1}", scenarioID, employeeID);
-                Results result = new Results();
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, cnn);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
-
-
-                if (ds.Tables.Count == 0)
-                {
-                    return result;
-                }
-                else
-                {
-                    foreach (DataRow dr in ds.Tables[0].Rows)
-                    {
-
-                    }
-                }
-            }
-        } */
-
-
-        private Boolean convertToResult(String boolString)
-        {
-            //Get the first Char of the parsed string
-            Char result = boolString.ToUpper().ToCharArray()[0];
-
-                
-            // Return True if 'Y' false other wise
-            return (result == 'Y');
-
-        }
-
-
-        //Convert a string to MD5
-        private string CalculateMD5Hash(string input)
-            {
-                // step 1, calculate MD5 hash from input
-                MD5 md5 = System.Security.Cryptography.MD5.Create();
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hash = md5.ComputeHash(inputBytes);
- 
-                // step 2, convert byte array to hex string
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hash.Length; i++)
-                {
-                    sb.Append(hash[i].ToString("X2"));
-                }
-                return sb.ToString();
-            }
-
-
-        // Returns the youtube video link.
-        private String getVideoLink(String bigString)
-        {
-            
-            String smallString;
-            Boolean found = false;
-            char[] seps = {'\"'};
-            String[] values = bigString.Split(seps);
-            int correctIndex = 0;
-
-            //Find the String after src
-            for(int i = 0; i<values.Length; i++)
-            {
-                if(values[i].Contains("src="))
-                {
-                    found = true;
-                    correctIndex = i + 1;
-                    break;
-                }
-            }
-
-            //Returns the video link
-            if(found)
-            {
-                smallString = values[correctIndex];
-                return smallString;
-            }
-            else
-                return "No Video Link Found";
-        }
-
-        //Get the scenario Tex
-        //May Change depeneding on Database changes
-        //Will get data from the pro quiz question table
-        private String getScenarionText(String bigString)
-        {
-            
-            // Start and End of the scenarion Text substring
-            int start,end,length;
-            String edit;
-
-
-            //Check if bigString contains "Scenarion (scenario Num)"
-            if(bigString.Contains("Background</strong>"))
-            {
-                //Find Start of substring
-                start = bigString.IndexOf("Background</strong>") + 17;
-
-                //Find end
-                end = bigString.Length - 4;
-
-                //Calculate length of scenarion text
-                length = end - start;
-
-                //Edit is the the scenaoio text with the html tags still in
-                edit = bigString.Substring(start,length).Trim();
-
-                edit.Replace("<p>","\n");
-                edit.Replace("</p>","\n");
-                edit.Replace("<strong>","");
-                edit.Replace("</strong>","");
-
-                return edit;
-
-
-            } 
-            // If no Scenario text can be found
-            else
-            {
-                return "No Scenario Text Found";
-            }
-        }
-
-
-        /// <summary>
-        ///   Returns the answers to a question
-        /// </summary>
-        /// <param name="bigString"></param>
-        /// <returns> String[] </returns>
-        private String[] getAnswers(String bigString)
-        {
-            //New String
-            String[] answers = new String[4];
-            int start, end, length;
-
-            //Asnwer A
-            start = bigString.IndexOf("a)");
-            end = bigString.IndexOf("\"",start);
-            length = end - start;
-            answers[0] = bigString.Substring(start, length);
-
-            //Asnwer B
-            start = bigString.IndexOf("b)");
-            end = bigString.IndexOf("\"", start);
-            length = end - start;
-            answers[1] = bigString.Substring(start, length);
-
-
-            //Asnwer C
-            start = bigString.IndexOf("c)");
-            end = bigString.IndexOf("\"", start);
-            length = end - start;
-            answers[2] = bigString.Substring(start, length);
-
-            //Asnwer D
-            start = bigString.IndexOf("d)");
-            end = bigString.IndexOf("\"", start);
-            length = end - start;
-            answers[3] = bigString.Substring(start, length);
-
-            //return
-            return answers;
-
-        }
-
-
-        //--------------------------------------------------------------------
-        //Returns a scenario
-        public Scenario returnScenario(int scenarioNum)
-        {
-
-            //Creating the connection string
-            String conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
-            //Accessing the database via the connection string
-
-            int idStart= 0;
-            String scenString = "Scenario " + scenarioNum;
-            Scenario s = new Scenario();
-            int id=0;
-            String vidLink;
-            String[] answers = new String[4];
-
-            using (MySqlConnection cnn = new MySqlConnection(conString))
-            {
-                //Question Object to be returned by method
-                Question[] retQuest = new Question[4];
-                String scenText = "";
-
-                cnn.Open();
-                String sql = String.Format("SELECT id, description from dgn6la8u0_frm_fields where name = \"{0}\"",scenString);
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, cnn);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
-
-                try{
-                    
-                    //Get Data from table
-                    foreach(DataRow dr in ds.Tables[0].Rows)
-                        {
-                            id = Convert.ToInt32(dr["id"]);
-                            idStart = id + 1;
-                            scenText = Convert.ToString(dr["description"]);
-                        }
-                
-                    //add to scenario
-                    s.scenarioID = id;
-                    s.scenarioInformation = getScenarionText(scenText);
-                    vidLink = getVideoLink(scenText);
-                    s.videoLink = vidLink;
-
-                
-                }
-                catch(Exception ex)
-                {
-                    //Do something
-                    s.scenarioInformation = ex.ToString();
-                }
-
-                sql = String.Format("SELECT name, options from dgn6la8u0_frm_fields where id in ({0},{1},{2},{3})",idStart ,idStart + 1,idStart + 2,idStart + 3);
-                da = new MySqlDataAdapter(sql, cnn);
-                ds = new DataSet();
-                da.Fill(ds);
-                int i = 0;
-                try{
-                    foreach(DataRow dr in ds.Tables[0].Rows)
-                    {
-                        retQuest[i].questionText = Convert.ToString(dr["name"]);
-                        answers = getAnswers(Convert.ToString(dr["options"]));
-                        retQuest[i].ansA = answers[0];
-                        retQuest[i].ansB = answers[1];
-                        retQuest[i].ansC = answers[2];
-                        retQuest[i].ansD = answers[3];
-                        i++;
-                    }
-                
-                    // Add questions to scenario
-                    s.questionArray = retQuest;
-
-                }
-                catch(Exception ex)
-                {
-                    //Do Something
-                    s.scenarioInformation = ex.ToString();
-                }
-
-                return s;
-            }
-        }
-
-
-
-            
-    }
-
 
     }
+}
